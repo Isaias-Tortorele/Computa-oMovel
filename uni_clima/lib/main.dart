c07153ce65e58add5b8c6d6bd0e4baebe12323ec
Isaias-Tortorele <isaiastortorele23@gmail.com>
2022-09-22 21:52:36 -0300

    first commit

---
+++ uni_clima/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const UniClima())
+}
+
+class UniClima extends StatelessWidget{
+  const UniClima({super.key});
+
+  @override 
+  Widget build(BuildContext context){
+    return Container()
+  }
+}
